2e98aec6409029dec34fa8e3d25fa2c96a4347a2
xshoji <xshoji@users.noreply.github.com>
2019-09-03 02:00:47 +0900

    update creator.sh

--- creator.sh
+++ creator.sh
@@ -10,10 +10,13 @@ cat << _EOT_
 Create unity project.
 
 Usage:
-  ./$(basename "$0") --name ui
+  ./$(basename "$0") --name ui [ --base template ]
 
 Required:
   -n, --name ui : Name of unity project.
+
+Optional:
+  -b, --base template : Name of base unity project. [ default: template ]
 
 Helper options:
   --help, --debug
@@ -37,6 +40,7 @@ do
     SHIFT="true"
     [[ "${ARG}" == "--debug" ]] && { shift 1; set -eux; SHIFT="false"; }
     { [[ "${ARG}" == "--name" ]] || [[ "${ARG}" == "-n" ]]; } && { shift 1; NAME="${1}"; SHIFT="false"; }
+    { [[ "${ARG}" == "--base" ]] || [[ "${ARG}" == "-b" ]]; } && { shift 1; BASE="${1}"; SHIFT="false"; }
     { [[ "${ARG}" == "--help" ]] || [[ "${ARG}" == "-h" ]]; } && { shift 1; HELP="true"; SHIFT="false"; }
     { [[ "${SHIFT}" == "true" ]] && [[ "$#" -gt 0 ]]; } && { shift 1; }
 done
@@ -45,6 +49,8 @@ done
 [[ -z "${NAME+x}" ]] && { printColored yellow "[!] --name is required.\n"; INVALID_STATE="true"; }
 # Check invalid state and display usage
 [[ -n "${INVALID_STATE+x}" ]] && { usage; }
+# Initialize optional variables
+[[ -z "${BASE+x}" ]] && { BASE="template"; }
 
 
 
@@ -57,14 +63,26 @@ cat << __EOT__
 [ Required parameters ]
 name: ${NAME}
 
+[ Optional parameters ]
+base: ${BASE}
+
 __EOT__
 
 PROJECT_DIR="$(cd $(dirname "${BASH_SOURCE:-$0}") && pwd)"
-TEMPLATE_DIR="${PROJECT_DIR}/template"
+TEMPLATE_DIR="${PROJECT_DIR}/${BASE}"
 TARGET_DIR="${PROJECT_DIR}/${NAME}"
 PROJECT_SETTING_PATH="${PROJECT_DIR}/${NAME}/ProjectSettings/ProjectSettings.asset"
 README_PATH="${PROJECT_DIR}/${NAME}/README.md"
 
-cp -R "${TEMPLATE_DIR}" "${TARGET_DIR}"
-sed -i "" "s/XXXXXXXXXXXXXXX/${NAME}/g" "${PROJECT_SETTING_PATH}"
-sed -i "" "s/XXXXXXXXXXXXXXX/${NAME}/g" "${README_PATH}"
+rsync -a "${TEMPLATE_DIR}/" "${TARGET_DIR}" \
+  --exclude 'Library/' \
+  --exclude 'obj/' \
+  --exclude '*.csproj' \
+  --exclude '*.sln' \
+  --exclude '*.sln' \
+  --exclude '.vs/'
+
+PLACEHOLDER="XXXXXXXXXXXXXXX"
+[[ ${BASE} != "template" ]] && { PLACEHOLDER="${BASE}" ;}
+sed -i "" "s/${PLACEHOLDER}/${NAME}/g" "${PROJECT_SETTING_PATH}"
+sed -i "" "s/${PLACEHOLDER}/${NAME}/g" "${README_PATH}"
