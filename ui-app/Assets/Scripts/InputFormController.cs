3598f3626df4ec68ff8366aa201ef1f551b4a321
xshoji <xshoji@users.noreply.github.com>
2019-10-01 01:40:03 +0900

    add add button, delete button

---
+++ ui-app/Assets/Scripts/InputFormController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFormController : MonoBehaviour
+{
+
+    public GameObject inputField;
+    public GameObject addButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
