1453c257cf25f06375b2f679cf4c590ed68c2e60
xshoji <xshoji@users.noreply.github.com>
2019-10-02 02:00:02 +0900

    add httprequest sample.

---
+++ httpclient/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public UnityEngine.UI.InputField inputField;
+
+    // > UnityでHTTPに接続する - Qiita  
+    // > https://qiita.com/ponchan/items/65aeb43e8fea8da0bcac
+    public void OnClick() {
+        Debug.Log("inputField.text: " + inputField.text);
+        Debug.Log("call ApiClient.Get!!");
+        // コルーチンを実行  
+        StartCoroutine("DoGet", inputField.text);
+    }
+
+    IEnumerator DoGet(string query)
+    {
+        string url = "https://itunes.apple.com/search?term=" + query + "&media=music";
+        Debug.Log("query: " + query);
+        Debug.Log("url: " + url);
+
+        UnityWebRequest request = UnityWebRequest.Get(url);
+
+        yield return request.SendWebRequest();
+        Debug.Log("get response!!");
+
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log("Error!");
+            Debug.Log(request.error);
+        }
+
+        Debug.Log("OK!");
+        Debug.Log(request.downloadHandler.text);
+
+    }
+}
---
+++ httpclient/README.md
@@ -0,0 +1,11 @@
+# httpclient
+
+httpclient
+
+# Requirement
+
+ - 2019.xxx
+
+# Dependencies
+
+Need to put following assets to `Assets/ExternalAssets` directory.
